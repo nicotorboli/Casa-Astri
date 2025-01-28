725f90aaef01ab50409549f4ffcbf3a7a24d29b6
nicotorboli <nicotorboli1@gmail.com>
2025-01-28 02:01:03 -0300

    llegan los productos y cat , pero no funciona bien el filtrado, ver lo que dice geminis

--- backend/settings.py
+++ backend/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Casa-Astri',
+        'USER': 'nicotorboli',  # Nombre del usuario que creaste
+        'PASSWORD': 'kjqlnico10',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
